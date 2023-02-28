e2e9cb85e9af84f905c0ee39e6685001a829368c
livongithub <berul526@newschool.edu>
2023-02-28 10:15:15 -0500

    commit

---
+++ index4.js
@@ -0,0 +1,27 @@
+// defining my variables 
+
+let numberOneVar = 0 
+let numberTwoVar = 0 
+let numberThreeVar = 0 
+
+
+let numberVar = numberOneVar + numberTwoVar + numberThreeVar
+
+let changeBackground = document.getElementById("changeBackground")
+let changeDiv = document.getElementById("changeDiv")
+let changeText = document.getElementById("changeText")
+let title = document.getElementById("title")
+
+
+
+changeBackground.addEventListener ()=> {
+
+}
+
+changeDiv.addEventListener ()=> {
+    
+}
+
+changeText.addEventListener ()=> {
+    
+}
