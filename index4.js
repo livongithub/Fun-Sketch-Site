d4d4271ee51b709c26dd1eaab7aca38cb1af6116
livongithub <berul526@newschool.edu>
2023-02-28 12:19:48 -0500

    commit

--- index4.js
+++ index4.js
@@ -4,24 +4,101 @@ let numberOneVar = 0
 let numberTwoVar = 0 
 let numberThreeVar = 0 
 
+let buttonOneBool = false 
+let buttonTwoBool = false 
+let buttonThreeBool = false 
 
-let numberVar = numberOneVar + numberTwoVar + numberThreeVar
+// catching the HTML elements 
 
 let changeBackground = document.getElementById("changeBackground")
 let changeDiv = document.getElementById("changeDiv")
 let changeText = document.getElementById("changeText")
 let title = document.getElementById("title")
+let bodyText = document.getElementById("bodyText")
 
 
 
-changeBackground.addEventListener ()=> {
 
-}
+// creating the actions when clicking 
 
-changeDiv.addEventListener ()=> {
+changeBackground.addEventListener('click', ()=>{
+    numberOneVar += 1 
+    //    checking in the console to see if it works 
+    console.log(numberOneVar)
+
+        // applying it in my HTML 
+        changeBackground.innerHTML = "change the backgrond    " + numberOneVar
+
+          // toggling a boolean
+          buttonOneBool =! buttonOneBool
+          console.log(buttonOneBool)
+  
+          if(buttonOneBool == true) {
+            document.body.style.backgroundColor = "rgb(255, 0, 127)"
+          }
+  
+          else {
+            document.body.style.backgroundColor = "rgb(204, 0, 204)"
+          }
+})
+
+
+
+changeDiv.addEventListener ("click", ()=>{
+    numberTwoVar += 1 
+    //    checking in the console to see if it works 
+       console.log(numberTwoVar)
     
-}
-
-changeText.addEventListener ()=> {
+        // applying it in my HTML 
+        changeDiv.innerHTML = "change the div    "+ numberTwoVar
     
-}
+        // toggling a boolean
+        buttonTwoBool =! buttonTwoBool
+        console.log(buttonTwoBool)
+
+        if(buttonTwoBool == true) {
+            changeDiv.style.backgroundColor = "rgb(102, 255, 178)"
+            changeDiv.style.width = "300px"
+            changeDiv.style.height = "300px"
+            changeDiv.style.borderRadius = "50%"
+        }
+
+        else {
+            changeDiv.style.backgroundColor = "rgb(255, 255, 102)"
+            changeDiv.style.width = "200px"
+            changeDiv.style.height = "200px"
+            changeDiv.style.borderRadius = "10px"
+        }
+})
+
+
+
+changeText.addEventListener ("click", ()=>{
+    numberThreeVar += 1 
+    //    checking in the console to see if it works 
+       console.log(numberThreeVar)
+    
+        // applying it in my HTML 
+        changeText.innerHTML = "read the secret message    "+ numberThreeVar
+
+          // toggling a boolean
+          buttonThreeBool =! buttonThreeBool
+          console.log(buttonThreeBool)
+  
+          if(buttonThreeBool == true) {
+              changeText.append("    boo!")
+              bodyText.append("   I scared you!")
+          }
+  
+          else {
+            changeText.append("")
+            bodyText.append("")
+          } 
+})
+
+// adding up the variables 
+
+let numberVar = numberOneVar + numberTwoVar + numberThreeVar
+
+
+title.innerHTML = "the buttons have been clicked   " + numberVar + "   times"
